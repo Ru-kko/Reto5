e3960f8b3269c6bcba22523ab5b3b4b4796b7182
Ru-kko <verase2003.1@gmail.com>
2021-10-03 21:34:47 -0500

    The constructor of the users table was finished and the construction of the input component for it was started.

--- src/crudLoader.js
+++ src/crudLoader.js
@@ -1,9 +1,19 @@
 function buildUsersTable(){
     $.ajax({
         type: 'GET',
-        url: 'https://g369d88cd72d64e-db202109232115.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/farm/farm',
+        url: 'https://g369d88cd72d64e-db202109232115.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client',
         success: (res) => {
-            console.log(res.items);
+            console.log(res);
+
+            for (var i = 0; i < res.items.length; i++) {
+                const row = '<tr class="tableRow">' +
+                    `<th scope="row" class="id">${res.items[i].id}</th>`  +
+                    `<th class="name">${res.items[i].name}</th>` + 
+                    `<th class="email">${res.items[i].email}</th>` +
+                    `<th class="age">${res.items[i].age}</th>` +
+                    '<td class="edit"><a class="btn btn-secondary btn-sm">edit</a></td>';
+                $('#userRows').append(row);
+            }
         }
     })
 }
